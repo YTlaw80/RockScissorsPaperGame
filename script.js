775d5f603df84eaf87b90817fad499a68e57c71d
taiyoung.jang <taiyoung.jang@kongstudios.net>
2022-07-21 19:14:58 +0900

    최고 점수(high_score) 변수와 최저 점수(low_score) 변수 제작 및 출력

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const $score = document.querySelector("#score"); // 점수 태그
 const $rock = document.querySelector("#scissors"); // 가위 버튼
 const $scissors = document.querySelector("#rock"); // 바위 버튼
 const $paper = document.querySelector("#paper"); // 보 버튼
+let high_score = 0;
+let low_score = 0;
 
 // 이미지 배열
 let img_url = ["Images/rock.png", "Images/scissors.png", "Images/paper.png"];
@@ -14,9 +16,9 @@ let computerChoice = 0;
 
 // 컴퓨터 이미지 모양 바꾸기
 function changeComputerHand() {
-    computerChoice++;
-    if(computerChoice > 2) computerChoice = 0;
-    $computer.src = `${img_url[computerChoice]}`;
+  computerChoice++;
+  if (computerChoice > 2) computerChoice = 0;
+  $computer.src = `${img_url[computerChoice]}`;
 }
 
 let computerInterval = setInterval(changeComputerHand, 50); // (실행할 함수, 주기)
@@ -26,42 +28,45 @@ let score = 0;
 
 // 사용자가 버튼 눌렀을 때 실행될 함수
 function clickButton(event) {
-    if (clickable) { // 다중 클릭 방지
-        clickable = false;
-        clearInterval(computerInterval);
-        // console.log(event.target.innerText);
-        const myChoise = event.target.innerText;
-        let resultMsg; // 결과 메시지
-        if(myChoise === "Scissors") {
-            if(computerChoice === 0) resultMsg = "you lose";
-            else if(computerChoice === 1) resultMsg = "A draw";
-            else resultMsg = "you win";
-        }else if(myChoise === "Rock") {
-            if(computerChoice === 0) resultMsg = "A draw";
-            else if(computerChoice === 1) resultMsg = "you win";
-            else resultMsg = "you lose";
-        } else { "Paper"
-            if(computerChoice === 0) resultMsg = "you win";
-            else if(computerChoice === 1) resultMsg = "you lose";
-            else resultMsg = "A draw";
-        }
-
-        // 점수 처리
-        if(resultMsg === "you win") score++;
-        else if(resultMsg === "you lose") score--;
-        else score = score;
-
-        // 점수, 결과 출력
-        $score.innerText = `${resultMsg}, Score : ${score}`;
-
-        // 다음번 실행은 2.5초후
-        setTimeout(() => {
-            clickable = true;
-            computerInterval = setInterval(changeComputerHand, 50); // 다시 이미지 변경 실행
-            $score.innerText = `Score : ${score}`;
-        }, 2500); // 단위 = ms
-
+  if (clickable) { // 다중 클릭 방지
+    clickable = false;
+    clearInterval(computerInterval);
+    // console.log(event.target.innerText);
+    const myChoise = event.target.innerText;
+    let resultMsg; // 결과 메시지
+    if (myChoise === "Scissors") {
+      if (computerChoice === 0) resultMsg = "you lose";
+      else if (computerChoice === 1) resultMsg = "A draw";
+      else resultMsg = "you win";
+    } else if (myChoise === "Rock") {
+      if (computerChoice === 0) resultMsg = "A draw";
+      else if (computerChoice === 1) resultMsg = "you win";
+      else resultMsg = "you lose";
+    } else {
+      "Paper"
+      if (computerChoice === 0) resultMsg = "you win";
+      else if (computerChoice === 1) resultMsg = "you lose";
+      else resultMsg = "A draw";
     }
+
+    // 점수 처리
+    if (resultMsg === "you win") score++;
+    else if (resultMsg === "you lose") score--;
+    else score = score;
+    if (score > high_score) high_score = score;
+    if (score < low_score) low_score = score;
+
+    // 점수, 결과 출력
+    $score.innerText = `${resultMsg}, Score : ${score}`;
+
+    // 다음번 실행은 2.5초후
+    setTimeout(() => {
+      clickable = true;
+      computerInterval = setInterval(changeComputerHand, 50); // 다시 이미지 변경 실행
+      $score.innerText = `Score : ${score} (high:${high_score}, low:${low_score})`;
+    }, 2500); // 단위 = ms
+
+  }
 }
 
 // 3개의 버튼 태그에 이벤트 리스너 설정
